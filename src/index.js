134a02cafc2d7bd455737a3c8fbeae026a2cd95a
ejazsidhu <ejazsidhu@gmail.com>
2020-04-18 19:03:10 +0500

    initail based setup for mcq game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './assets/style.css';
+import questionService from './quizService'
+class QuizBee extends Component {
+    state={
+        questionBank:[]
+    };
+    
+    render() { 
+        return ( 
+            <div className="container">
+                <div className="title">Quiz Bee</div>
+            </div>
+         );
+    }
+}
+ 
+ReactDOM.render(<QuizBee/>,document.getElementById("root"))
